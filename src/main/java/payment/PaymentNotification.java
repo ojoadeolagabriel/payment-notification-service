89e62c5017f57ed40512a8725dd5a90772180ac6
adeola.ojo <Adeola.Ojo@Interswitchng.com>
2016-07-16 21:10:34 +0100

    more info details

--- src/main/java/payment/PaymentNotification.java
+++ src/main/java/payment/PaymentNotification.java
@@ -1,9 +1,35 @@
 package payment;
 
+import org.joda.time.DateTime;
+
 /**
  * Created by Adeola.Ojo on 7/16/2016.
  */
 public class PaymentNotification {
 
+    private long paymentLogId;
+    private long billerId;
+    private long paymentItemId;
+    private long institutionId;
+    private double initialTotalAmount;
+    private String additionalInformation;
+    private String cardPan;
+    private String paymentReference;
+    private String merchantPaymentReference;
+    private String ftpUrl;
+    private String ftpUrlUsername;
+    private String ftpUrlPassword;
+    private String collectionsAccountNumber;
+    private String channelDescription;
+    private String customerNumber;
+    private String customerName;
+    private String isoFee;
+    private String isoFeePercent;
+    private String paymentCurrencyCode;
+    private String location;
+    private DateTime paymentDate;
+    private boolean allowServiceNotification;
+    private boolean allowBankCode;
+    private boolean isDoublePosting;
 
 }
