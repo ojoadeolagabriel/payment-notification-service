d3825ca4551533f057d32ff243b3026f5c85c9d0
adeola.ojo <Adeola.Ojo@Interswitchng.com>
2016-07-17 05:54:33 +0100

    cron

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,10 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * Created by Adeola.Ojo on 7/16/2016.
  */
+@SpringBootApplication
+@EnableScheduling
 public class Program {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Program.class);
     }
 
 }
